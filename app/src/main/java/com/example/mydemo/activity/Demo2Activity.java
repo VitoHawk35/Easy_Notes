88cffa59dce42c3da0743b8e724e5bfadfec03a9
Vito Kernal35 <hkj35@iCloud.com>
2025-11-22 05:04:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mydemo/activity/Demo2Activity.java
@@ -0,0 +1,16 @@
+package com.example.mydemo.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mydemo.R;
+
+public class Demo2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo2);
+    }
+}
