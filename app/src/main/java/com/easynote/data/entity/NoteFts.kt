b4ce175bf72aaaf59dfbfeb062de143b0a25f5f8
七飒 <sunjian9250@163.com>
2025-12-03 12:16:32 +0800

    合并部分冗余接口、新增按照指定排序

--- app/src/main/java/com/easynote/data/entity/NoteFts.kt
+++ app/src/main/java/com/easynote/data/entity/NoteFts.kt
@@ -5,14 +5,19 @@ import androidx.room.Entity
 import androidx.room.Fts4
 
 @Fts4
-@Entity(tableName = "note_content_search")
-data class NoteContentSearch(
+@Entity(tableName = "note_fts")
+data class NoteFts(
 
     @ColumnInfo(name = "page_index")
     var pageIndex: Int,
 
     @ColumnInfo
-    var content: String? = null
+    var title: String,
 
+    @ColumnInfo
+    var summary: String,
+
+    @ColumnInfo
+    var content: String? = null
 
 )
--- app/src/main/java/com/easynote/data/repository/Repository.kt
+++ app/src/main/java/com/easynote/data/repository/Repository.kt
@@ -4,8 +4,7 @@ import android.content.Context
 import android.net.Uri
 import androidx.paging.PagingData
 import com.easynote.data.annotation.NoteOrderWay
-import com.easynote.data.annotation.ORDER_UPDATE_TIME_DESC
-import com.easynote.data.entity.NoteEntity
+import com.easynote.data.annotation.UPDATE_TIME_DESC
 import com.easynote.data.entity.TagEntity
 import com.easynote.data.relation.NoteWithTags
 import kotlinx.coroutines.flow.Flow
@@ -76,9 +75,9 @@ interface Repository {
      * Update the abstract of a note.
      *
      * @param noteId The ID of the note.
-     * @param abstract The new abstract for the note.
+     * @param summary The new abstract for the note.
      */
-    suspend fun updateAbstract(noteId: Long, abstract: String)
+    suspend fun updateTitleOrTitle(noteId: Long, title: String, summary: String)
 
     /**
      * Save an image associated with a note.
@@ -123,26 +122,27 @@ interface Repository {
         tagIds: Set<Long>? = null,
         startTime: Long? = null,
         endTime: Long? = null,
-        @NoteOrderWay orderWay: String? = ORDER_UPDATE_TIME_DESC
+        @NoteOrderWay orderWay: String? = UPDATE_TIME_DESC
     ): Flow<PagingData<NoteWithTags>>
 
     /**
-     * Get notes by a set of tag IDs.
+     * Get the count of notes associated with specific tag IDs.
      *
      * @param tagIds The set of tag IDs to filter notes.
-     * @param orderWay The order way for sorting notes.
-     * @return A Flow emitting a list of NoteWithTags that match the tag IDs.
+     * @return The count of notes that match the tag IDs.
      */
-    fun getNoteByTags(
-        tagIds: Set<Long>? = emptySet(),
-        pageSize: Int,
-        @NoteOrderWay orderWay: String? = ORDER_UPDATE_TIME_DESC
-    ): Flow<PagingData<NoteEntity>>
+    fun getAllNoteWithTagsFlow(
+        query: String? = null,
+        tagIds: Set<Long>? = null,
+        startTime: Long? = null,
+        endTime: Long? = null,
+        @NoteOrderWay orderWay: String? = UPDATE_TIME_DESC
+    ): Flow<List<NoteWithTags>>
 
     /**
-     * Get all tags as a flow list.
+     * Get all tags as a paging flow.
      *
-     * @return A Flow emitting a list of TagEntity.
+     * @return A Flow emitting PagingData of TagEntity.
      */
     fun getAllTagsFlow(pageSize: Int): Flow<PagingData<TagEntity>>
 
@@ -156,14 +156,6 @@ interface Repository {
     suspend fun getNoteContentByIdAndPageIndex(noteId: Long, pageIndex: Int): String?
 
     /**
-     * Get the count of notes associated with specific tag IDs.
-     *
-     * @param tagIds The set of tag IDs to filter notes.
-     * @return The count of notes that match the tag IDs.
-     */
-    suspend fun getNoteCountByTags(tagIds: Set<Long>): Int
-
-    /**
      * Get a note along with its associated tags by note ID.
      *
      * @param noteId The ID of the note.
@@ -171,13 +163,6 @@ interface Repository {
      */
     suspend fun getNoteWithTagsById(noteId: Long): NoteWithTags?
 
-    /**
-     * Search notes by a query string.
-     *
-     * @param query The search query.
-     * @return A Flow emitting a list of NoteWithTags that match the query.
-     */
-    suspend fun searchNotesByQuery(query: String, pageSize: Int): Flow<PagingData<NoteWithTags>>
 
     /**
      * Modify the order way of notes.
